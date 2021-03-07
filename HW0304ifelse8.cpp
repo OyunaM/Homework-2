4aeb6512024e1048d58089c97a237ab46e66f726
OyunaM <79499310+OyunaM@users.noreply.github.com>
2021-03-06 23:01:04 -0800

    Update HW0304ifelse8.cpp

--- HW0304ifelse8.cpp
+++ HW0304ifelse8.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main(){
-	char ch,a,e,i,o,u;
+	char ch;
 	cout<<"Input ch : ";
 	cin>>ch;
 	
 	
-	if(ch==a,ch==e,ch==i,ch=o,ch=u){ 
+	if(ch=='a' || ch=='e' || ch=='i' || ch='o' || ch='u'){ 
 		cout<<ch<<" is vowel";
 	}else{
 		cout<<ch<<" is consonant";
