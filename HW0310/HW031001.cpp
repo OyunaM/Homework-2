6659ad58570921ef7e69a0fe87ffc037ad582ae4
OyunaM <79499310+OyunaM@users.noreply.github.com>
2021-03-11 21:29:18 -0800

    Add files via upload

---
+++ HW0310/HW031001.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int i=1,n;
+	
+	cout<<"Enter n : ";
+	cin>>n;
+	
+	while (i<=n){
+	cout<<("%d" ,i);
+	i=i+1;
+}	
+	
+	
+	
+	return 0;
+}
---
+++ HW0310/HW031002.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int i=1,n;
+	
+	cout<<"Enter n : ";
+	cin>>n;
+	
+	while (i>=1){
+	cout<<("%d" ,n);
+	i=n--;
+}	
+	
+	
+	
+	return 0;
+}
