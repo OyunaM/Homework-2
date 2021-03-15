fb41601ed1f726535b01a355bb64c5f57081924e
OyunaM <79499310+OyunaM@users.noreply.github.com>
2021-03-14 19:22:41 -0700

    Add files via upload

---
+++ HW0312/HW0312w3r03.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int num,sum=0;
+	
+	cout<<"Enter num : ";
+	cin>>num; 
+	
+	for(int i = 1; i<=num ; i++){
+		cout<<i<<" "<<endl;
+}
+			
+	for(int i = 1; i<=num ; i++){
+		
+			sum = sum + i;
+		}
+	
+			cout<<"Total = "<<sum;
+				
+	
+	
+	
+	return 0;
+}
